55395c8f2b532ae4e700980344a83fa441f1871f
lagebj <lage.berger.jensen@outlook.com>
2021-08-10 09:52:45 +0200

    remove commented lines

--- Phelpers.psm1
+++ Phelpers.psm1
@@ -1,8 +1,8 @@
 ﻿foreach ($Folder in @('classes', 'scripts\private', 'scripts\public')) {
     [string] $Root = Join-Path -Path $PSScriptRoot -ChildPath $Folder
+
     if (Test-Path -Path $Root) {
-        # $Files = Get-ChildItem -Path $root -Filter *.ps1 -Recurse
-        Get-ChildItem -Path $Root -Filter *.ps1 -Recurse | Where-Object Name -NotLike '*.Tests.ps1' | ForEach-Object {. $_.FullName}
+        Get-ChildItem -Path $Root -Filter '*.ps1' -Recurse | Where-Object -Property 'Name' -NotLike '*.Tests.ps1' | ForEach-Object {. $_.FullName}
     }
 }
 
